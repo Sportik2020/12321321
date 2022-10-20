591975205d30fa9227106baa65edca516c433382
Sportik2020 <114169865+Sportik2020@users.noreply.github.com>
2022-10-20 18:33:01 +0300

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//18
+	/*int n;
+	cin >> n;
+	int i = 1;
+	int k = 0;
+	while (i <= n) {
+		if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0)
+		{
+			k++;
+		}
+		i += 1;
+	}
+	cout << k << endl;*/
+//1
+/*	int i = 1;
+	while (i < 99) {
+		if (i % 7 == 0) {
+			cout << i << " " ;
+		}
+		i += 1;
+	}*/
+//2
+/*	int n;
+	cin >> n;
+	int i = 1;
+	while (1 <= n) {
+		if (i <= n) {
+			cout << '$';
+		}
+		i += 1;
+	}*/
+//3
+/*	int a = 0;
+	int i = 20;
+	while (i <=78) {
+		if (i % 2 == 0) {
+			a = a + i;
+		}
+		i += 1;
+	}
+	cout << a;*/
+//4
+/*	int a, b;
+	int sum = 1;
+	cin >> a >> b;
+	int i = a;
+	while (i <= b) {
+		if (i % 9 == 2 || i % 9 == 3) {
+			sum = sum * i;
+		}
+		i += 1;
+	}
+	cout << sum << endl;*/
+//5
+/*	int a, b;
+	cin >> a >> b;
+	int i = b;
+	while (i > a) {
+		if (i % 7 == 0) {
+			cout << i<<" ";
+		}
+		i -= 1;
+	}
+	cout << i <<" "<< endl;*/
+
+
+
+
+
+
+
+
+	return 0;
+}
